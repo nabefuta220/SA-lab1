2d3a10aa789d852092ac70ad87971cc7cd805272
nabefuta <satoshinabe@220@gmail.com>
2021-10-26 23:36:44 +0900

    created task1-2

---
+++ task1-2.py
@@ -0,0 +1,13 @@
+from src.getSample import get_data
+
+if __name__ == "__main__":
+    data=get_data()
+    mean=data.mean(numeric_only=True)
+    var=data.var(numeric_only=True)
+    std=data.std(numeric_only=True)
+    corr=data.corr()
+    print(f"mean:\n{mean}")#平均
+    print(f"var:\n{var}")#分散
+    print(f"std:\n{std}")#標準偏差
+    print(f"corr:\n{corr}")#相関係数
+    
