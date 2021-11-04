824e34eed2c8f940323ef539fcd8b39cf625523a
nabefuta <satoshinabe@220@gmail.com>
2021-11-04 21:04:24 +0900

    leved sub methord

--- src/getSample.py
+++ src/getSample.py
@@ -1,20 +1,24 @@
 """URLからn個分のデータを取得する"""
-from . import URL,data_file as DF
 import pandas as pd
 import requests
-def get_sample(n:int=1000):
+
+from . import URL
+from . import data_file as DF
+
+
+def get_sample(n:int=1000)->str:
     """
     URLからn個分のデータを取得する
     
     Parameters
     ----------
-    n : int ,defalt=1000
+    n : int , defalt=1000
         取得するデータの個数
     
     Returns
     -------
-    str :str
-    取得したデータ(生のテキストファイル)
+    str : str
+        取得したデータ(生のテキストファイル)
     """
     res=requests.get(f"{URL}&n={n}")
     print(res)
@@ -22,9 +26,19 @@ def get_sample(n:int=1000):
     return res.text
 
 
-def get_data(data_file=DF):
+def get_data(data_file:str=DF)->pd.DataFrame:
     """
     データファイルからpandasファイルに変換する
+
+    Parameters
+    ----------
+    data_file : str , defalt = data_file
+        取得したいデータのパス
+
+    Returns
+    -------
+    data : pandas.Dataframe
+        データ
     """
     csv_input = pd.read_csv(filepath_or_buffer=data_file,  sep=",")
     return csv_input
--- src/writeFile.py
+++ src/writeFile.py
@@ -1,11 +1,23 @@
 """
 データをcsvファイルに書き込む
 """
-import pandas as pd
 import csv
 import os
 
-def write_file(datas:pd,file):  
+import pandas as pd
+
+
+def write_file(datas:pd.DataFrame,file:str):  
+    """
+    データをscvファイルに保存する
+
+    Parameters
+    ----------
+    datas : pandas.Dataframe
+        データ
+    file : str
+        保存するファイルのパス
+    """
     os.makedirs(os.path.dirname(file), exist_ok=True)
     datas.to_csv(file,index=False)
-    return
+
