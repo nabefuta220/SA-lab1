967eabc5947f9b5780b224320fbed510f6119522
nabefuta <satoshinabe@220@gmail.com>
2021-11-07 13:43:47 +0900

    changed phothesis

--- task2-1.py
+++ task2-1.py
@@ -26,22 +26,22 @@ def linked_phothesis(data:pd.DataFrame,index:List[str],significance_level : floa
 
     Returns
     ------
-    (res,t_value)
-    res: bool
+    accepted: bool
         2つの量的データに差があるか
-        Trueならば 2つの量的データに差がないとはいえない
+        True ならば2つの量的データに差がないとはいえない
         Falseならば2つの量的データに差がないと言える
     prob: float
         t-統計量
     """
     #データを抽出する
-    ndata=data[index].diff(axis=1).dropna(axis=1)
+    data1=data[index[0]]
+    data2=data[index[1]]
     
-    # 1標本の検定を行う
-    t,p=stats.ttest_1samp(ndata, 0)
-    p=p[0]
-    accept = p >(significance_level/2)
-    print(f"{index[0]} vs {index[1]}: \n t-value : {t[0]} \n p-value : {p} \n -> {'accepted' if accept else 'rejected'}")
+    #対応がある2変数の検定を行う
+    t,p=stats.ttest_rel(data1,data2)
+
+    accept = p >significance_level
+    print(f"{index[0]} vs {index[1]}: \n t-value : {t} \n p-value : {p} \n -> {'accepted' if accept else 'rejected'}")
 
     return (accept,t)
 
@@ -49,7 +49,6 @@ def unlinked_phothesis(data:pd.DataFrame,varable: str,index:List[str],significan
     """
     対応がないデータについて、両側検定によって、2つのグループのデータに差があるか判定する
 
-    
     Parameters
     ----------
     data : pandas.Dataframe
@@ -63,32 +62,43 @@ def unlinked_phothesis(data:pd.DataFrame,varable: str,index:List[str],significan
 
     Returns
     ------
-    res: bool
-        2つの量的データに差があるか
-        Trueならば 2つの量的データに差がないとはいえない
-        Falseならば2つの量的データに差がないと言える
-    prob: float
-        t-統計量
+    res: pandas.DataFrame
+        各量的データとt統計量、差があるかをまとめたもの
+        indexは次の通り:
+
+        accepted: bool
+            2つの量的データに差があるか
+            Trueならば 2つの量的データに差がないとはいえない
+            Falseならば2つの量的データに差がないと言える
+        prob: float
+            t-統計量
+
+        columnはdataのcolumnのうち、量的データであるもの
+
+    Raises
+    ------
+    KeyError
+        indexの値がvarableにないとき
     """
     #データを抽出する
     indexs=data.select_dtypes(include='number').columns
     data1 = data[data[varable]== index[0]].select_dtypes(include='number')
     data2 = data[data[varable]== index[1]].select_dtypes(include='number')
 
-
-
+    #データが存在しているか確認する
     if len(data1)==0:  
         raise KeyError(index[0])
     if len(data2)==0:  
         raise KeyError(index[1])
     
+    #対応がない2変数の検定を行う
     t_value,prob=stats.ttest_ind(data1,data2)
-    print(t_value)
-    print(prob)
+
+    #仮説を採択するか判断する
     res=pd.DataFrame([t_value,[True if i > significance_level else False for i in prob]])
     res.columns=indexs
     res.index=['t_value','accepted']
-    print(res)
+
     print(f"{index[0]} vs {index[1]}:\n")
     for j,i in enumerate(indexs):
         print(f"({i}):\n t-value : {t_value[j]}\n p-value : {prob[j]} \n-> {'accepted' if res.loc['accepted',i] else 'rejected'} ")
@@ -99,10 +109,19 @@ if __name__=='__main__':
     data=get_data(data_file='output/hypothesis.csv')
     data_column=data.select_dtypes(include='number').columns
 
+    #対応ありの仮説検定
     print("linked hyothesis:\n")
     #2科目を抽出する
     for i in range(len(data_column)):
         for j in range(i+1,len(data_column)):
             linked_phothesis(data,[data_column[i],data_column[j]],0.05)
+
+    #対応なしの仮説検定
     print("unlinked hyothesis:\n")
-    unlinked_phothesis(data,'parental level of education',['high school', 'some college'],0.05)
+    #親の出身校でグループ分けする
+    data_column=data['parental level of education'].unique()
+    print(data_column)
+    #2群を抽出する
+    for i in range(len(data_column)):
+        for j in range(i+1,len(data_column)):
+            unlinked_phothesis(data,'parental level of education',[data_column[i],data_column[j]],0.05)
